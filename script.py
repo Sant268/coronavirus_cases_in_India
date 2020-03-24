a9ebbcd629299ae1861e893f824fb66c29085512
Kunal Booch <kunalbooch@hotmail.com>
2020-03-24 18:00:49 +0530

    Done

--- script.py
+++ script.py
@@ -1,7 +1,15 @@
 import requests
-from BS4 import BeautifulSoup4
+from bs4 import BeautifulSoup
 URL = "https://www.mohfw.gov.in/"
 page = requests.get(URL)
-soup = BeautifulSoup4(page.content, 'html parser')
-results = soup.find(class_='icount')
-print(results.text)
+soup = BeautifulSoup(page.content, 'html.parser')
+result = soup.find_all(class_="icount")
+print("Active Values are:")
+print(result[1].text)
+print("Recovered Patients:")
+print(result[2].text)
+print("Deaths:")
+print(result[3].text)
+
+
+
