93e7a7efc5ade48e0746c98b82e6f133a86e2700
Kunal Booch <kunalbooch@hotmail.com>
2020-04-01 15:42:10 +0530

    Updated after changes to MOHFW

--- script.py
+++ script.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup
 URL = "https://www.mohfw.gov.in/"  #Indian Gov Website
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
-result = soup.find_all(class_="icount")
-print("Active Values are:")
-print(result[1].text)
+active = soup.find(class_="bg-blue")
+cure = soup.find(class_="bg-green")
+death = soup.find(class_="bg-red")
+print("Active Patients:")
+print(active.text[:6])
 print("Recovered Patients:")
-print(result[2].text)
+print(cure.text[:5])
 print("Deaths:")
-print(result[3].text)
+print(death.text[:5])
 
 
 
